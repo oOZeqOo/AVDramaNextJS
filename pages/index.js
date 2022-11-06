d1dfe7032fb6a4e84df1ab9923ba1396caa36811
oOZeqOo <55514489+oOZeqOo@users.noreply.github.com>
2022-11-06 15:02:49 -0600

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>This is an example for the AV Drama Migration</h1>
+    </div>
+  );
+}
