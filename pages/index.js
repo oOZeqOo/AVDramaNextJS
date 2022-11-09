2defc1007dde1c0b0df3556d57e548fb57842bbf
oOZeqOo <55514489+oOZeqOo@users.noreply.github.com>
2022-11-08 23:59:47 -0600

    timeline

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
+import Timeline from "../src/components/Timeline";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>This is an example for the AV Drama Migration</h1>
-    </div>
-  );
+  return <Timeline />;
 }
