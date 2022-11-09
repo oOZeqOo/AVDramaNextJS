97e6867956ed2bf03c5f5a6827b9a6c49ea716be
oOZeqOo <55514489+oOZeqOo@users.noreply.github.com>
2022-11-09 01:28:42 -0600

    temp

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,12 @@
 /**
  * @type {import('next').NextConfig}
  */
-
- const isGithubActions = process.env.GITHUB_ACTIONS || false;
- const repo = "/AVDramaNextJS";
- let assetPrefix = "";
- let basePath = "/";
-
- if (isGithubActions) {
-   // trim off `<owner>/`
-   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-   assetPrefix = `/${repo}`;
-   basePath = `/${repo}`;
- }
-
- const nextConfig = {
-   images: {
-     loader: "akamai",
-     path: basePath,
-   },
-   assetPrefix: assetPrefix,
-   basePath: basePath,
- };
+const nextConfig = {
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  assetPrefix: "./",
+};
 
 export default nextConfig;
