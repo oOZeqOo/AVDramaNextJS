02ded6881365c098224e0603f13d48e55e59a78a
oOZeqOo <55514489+oOZeqOo@users.noreply.github.com>
2022-11-09 00:08:25 -0600

    timeline

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,32 @@
-import Timeline from "../src/components/Timeline";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <Timeline />;
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        textAlign: "center",
+      }}
+    >
+      <h1>AVDrama with Next js</h1>
+      <div
+        style={{
+          height: 50,
+          backgroundColor: "red",
+          borderRadius: 20,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+      >
+        <Link href="/timeline">Go to timeline</Link>
+      </div>
+    </div>
+  );
 }
