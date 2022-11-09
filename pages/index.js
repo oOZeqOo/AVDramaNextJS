2cd5960844fa62b645c5710505044495d45d7739
oOZeqOo <55514489+oOZeqOo@users.noreply.github.com>
2022-11-09 00:36:20 -0600

    temp

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,6 @@
-import Link from "next/link";
+import HomePage from "../src/components/HomePage";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        textAlign: "center",
-      }}
-    >
-      <h1>AVDrama with Next js</h1>
-      <div
-        style={{
-          height: 50,
-          backgroundColor: "red",
-          borderRadius: 20,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          textAlign: "center",
-        }}
-      >
-        <Link href="/timeline">Go to timeline</Link>
-      </div>
-    </div>
-  );
+  return <HomePage />;
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Link from "next/link";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        textAlign: "center",
+      }}
+    >
+      <h1>AVDrama with Next js</h1>
+      <div
+        style={{
+          height: 50,
+          backgroundColor: "red",
+          borderRadius: 20,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+      >
+        <Link href="./timeline">Go to timeline</Link>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
