2c3519657ca40f8056d12198696cab8c206cb7ff
oOZeqOo <55514489+oOZeqOo@users.noreply.github.com>
2023-04-08 23:25:12 -0500

    Randomized images

--- src/components/image_gallery/ImageGridContainer.js
+++ src/components/image_gallery/ImageGridContainer.js
@@ -88,12 +88,12 @@ const ImageGridContainer = () => {
   }, [gridItemsWidth]);
 
   function shuffleArray(array) {
-    // for (var i = array.length - 1; i > 0; i--) {
-    //   var j = Math.floor(Math.random() * (i + 1));
-    //   var temp = array[i];
-    //   array[i] = array[j];
-    //   array[j] = temp;
-    // }
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
     console.log(array?.length);
     console.log([...new Set(array)]?.length);
     return array;
