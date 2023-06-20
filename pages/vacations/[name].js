e2373e701e7ee37e2ddae0d82d9d3aa8cd352200
oOZeqOo <55514489+oOZeqOo@users.noreply.github.com>
2023-06-20 14:31:16 -0500

    Anna birthday 2023 update

--- pages/vacations/[name].js
+++ pages/vacations/[name].js
@@ -24,6 +24,7 @@ const Card = ({
           style={{
             display: "flex",
             justifyContent: "center",
+            alignItems: "middle",
           }}
         >
           <Image
--- src/assets/data/VacationData.js
+++ src/assets/data/VacationData.js
@@ -19,98 +19,80 @@ export const vacationData = {
     cards: [
       {
         title: "Leaving to go to Vietnam ✈✈ ",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/flight_leave_san_antonio.jpg",
       },
       {
         title: "Anna's favorite milk tea 🥛🍵",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/koi.jpg",
       },
 
       {
         title: "Beautiful woman 😍😍😍",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/anna_bac_xiu.JPG",
       },
       {
         title: "Fun at the Louisian",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/louisian.JPG",
       },
       {
         title: "Beach pic 😎💘",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/interesting.JPG",
       },
 
       {
         title: "Beautiful girl in the garden 🌹🌹🌹",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/anna_flowers.JPG",
       },
       {
         title: "Woo Wooooooo 🌸🌸🌸🌸",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/anna_flowers_2.JPG",
       },
       {
         title: "Anna in front of a boat! ⛵⛵",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/anna_pirate.jpg",
       },
       {
         title: "Close up of Anna in front of a boat",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/anna_pirate_close.JPG",
       },
       {
         title: "Beautiful girl with an ocean view 💖💖",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/anna_sitting.jpg",
       },
       {
         title: "Portrait with golden hour 😘🤗😍",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/anna_sunflower.JPG",
       },
       {
         title: "Night pictures on a building 📸",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/building_night.JPG",
       },
       {
         title: "Arts and crafts with the couple 😘😘😘😘",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/doramon.jpg",
       },
 
       {
         title: "Love in a garden 🌷🌹🌼🌻💐💖💖💖",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/flower_garden_love.jpg",
       },
 
       {
         title: "MOOOOAAAHHH  💋💋💋💋💋💋",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/kiss.jpg",
       },
 
       {
         title: "Another MOOOOAAAHHH  💋💋💋💋💋💋",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/lake_side.jpg",
       },
 
       {
         title: "Lunar New Year Tree 🌴🌴🌴",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/lunar_tree.jpg",
       },
       {
         title: "Motorbike couple 🛵🛵🛵",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/motorbike.jpg",
       },
       {
@@ -120,57 +102,46 @@ export const vacationData = {
       },
       {
         title: "Anna's favorite: Phuc Long 💖💖💖",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/phuc_long.jpg",
       },
       {
         title: "Boat kiss 😘😘😘",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/pirate_close.jpg",
       },
       {
         title: "Beach House Pool Time 🏊‍♀️🏊‍♂️",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/pool_1.jpg",
       },
       {
         title: "Steam bun for the steam bun 🍞🍞🍞",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/steam_bun_2.jpg",
       },
       {
         title: "VinPearl Couple 👫",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/vinpearl.jpg",
       },
       {
         title: "VinPearl Castle 🏰",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/vinpearl_2.jpg",
       },
       {
         title: "Wood souvenir 🪓🪓",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/woodshop.jpg",
       },
       {
         title: "Steam Bun 😎",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/zac.JPG",
       },
       {
         title: "Zac Leaving Vietnam 😭",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/zac_go_back.jpg",
       },
       {
         title: "Going Back to Ho Chi Minh city 🛫🛬",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/zac_go_back_flight.jpg",
       },
       {
         title: "Anna's family picture with Zac",
-        description: "",
         imgPath: "/images/vacations/vietnam_1/anna_family.JPG",
       },
     ],
