d36dae1955c477b7b94d19be02acf60f4e75d6da
oOZeqOo <zax976@gmail.com>
2024-05-19 15:38:18 -0500

    Addedf images

--- make.bat
+++ make.bat
@@ -3,10 +3,10 @@
 SET command=%1
 
 @REM Decide what to do from this Windows Equivalent of a makefile
-if %command%==all GOTO :ALL
+if %command%==start GOTO :START
 GOTO :END
 
-:ALL
+:START
     START "START" CMD /K "npm run dev"
     timeout /t 5 && start chrome http://localhost:3000/
     GOTO :END
